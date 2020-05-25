2b587960086f177ccdd2bc6d75c8fd02d63481ed
harpercham <43534952+harpercham@users.noreply.github.com>
2020-05-25 18:24:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var groupAdmin = "+6590970960"; // TODO: Specify the WhatsApp number of the group creator, including the country code
+var groupName = "Test";   // TODO: Specify the name of the group
+var message = "hello"
+
+
+var http = require('http');
+
+var instanceId = "24";  // TODO: Replace it with your gateway instance ID here
+var clientId = " maxchao7023@gmail.com";  // TODO: Replace it with your Forever Green client ID here
+var clientSecret = "16609007ee7840138d521bdab65986b6";   // TODO: Replace it with your Forever Green client secret here
+
+var jsonPayload = JSON.stringify({
+    group_admin: groupAdmin, // TODO: Specify the WhatsApp number of the group creator, including the country code
+    group_name: groupName,   // TODO:  Specify the name of the group
+    message: message  // TODO: Specify the content of your message
+});
+
+var options = {
+    hostname: "api.whatsmate.net",
+    port: 80,
+    path: "/v3/whatsapp/group/text/message/" + instanceId,
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "X-WM-CLIENT-ID": clientId,
+        "X-WM-CLIENT-SECRET": clientSecret,
+        "Content-Length": Buffer.byteLength(jsonPayload)
+    }
+};
+
+var request = new http.ClientRequest(options);
+request.end(jsonPayload);
+
+request.on('response', function (response) {
+    console.log('Heard back from the WhatsMate WA Gateway:\n');
+    console.log('Status code: ' + response.statusCode);
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+        console.log(chunk);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
